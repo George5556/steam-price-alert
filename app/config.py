e9b05aae9350ab60d4f73f8ca507d8c939bc2352
George5556 <durochok11112@gmail.com>
2026-06-15 20:44:38 +0300

    feat: add application settings

--- app/config.py
+++ app/config.py
@@ -1,1 +1,14 @@
-TODO
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    bot_token: str
+    database_url: str
+    itad_api_key: str
+    check_interval_hours: int = 3
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
