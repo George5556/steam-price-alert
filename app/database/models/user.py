6dc374100a04ffa6b0287476a35fd2df5eda8c94
George5556 <durochok11112@gmail.com>
2026-06-15 20:50:13 +0300

    feat: add user model

--- app/database/models/user.py
+++ app/database/models/user.py
@@ -1,1 +1,15 @@
+from datetime import datetime
 
+from sqlalchemy import BigInteger, DateTime, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database.models.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True)
+    username: Mapped[str | None] = mapped_column(String(255), nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
