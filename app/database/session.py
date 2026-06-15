f372e3dfc1dcbe85cb4ff0fd111870284be0b34e
George5556 <durochok11112@gmail.com>
2026-06-15 20:48:09 +0300

    feat: add database session

--- app/database/session.py
+++ app/database/session.py
@@ -1,1 +1,6 @@
-TODO
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from app.config import settings
+
+engine = create_async_engine(settings.database_url)
+async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
